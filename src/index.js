c8dadbd5fcab4f70d788745ef77af0bc720cf4d3
ornery-mouse <cory.sorel@gmail.com>
2022-01-18 09:38:52 -0600

    prep for heroku deployment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql",
+  uri: "http://print-shop-server.herokuapp.com/graphql",
   cache: new InMemoryCache()
 })
 
